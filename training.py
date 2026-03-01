4efeb09060a6453201b73b39d4ed1f93517ba68d
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2026-03-01 19:08:36 +0530

    updated batch_size

--- training.py
+++ training.py
@@ -137,7 +137,7 @@ def train_model():
 
     train_loader = DataLoader(
         train_ds,
-        batch_size=64,
+        batch_size=128,
         shuffle=True,
         num_workers=0,      # Windows safe
         pin_memory=False    # CPU safe
@@ -145,7 +145,7 @@ def train_model():
 
     val_loader = DataLoader(
         val_ds,
-        batch_size=64,
+        batch_size=128,
         shuffle=False,
         num_workers=0,
         pin_memory=False
