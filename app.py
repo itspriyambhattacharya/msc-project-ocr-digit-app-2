924063b739777fb2f9cd4dad865ad12453e07f09
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2026-03-01 13:27:56 +0530

    fixed error

--- app.py
+++ app.py
@@ -43,8 +43,11 @@ model = None
 
 
 class PriyamDigitNet(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes=10):
         super(PriyamDigitNet, self).__init__()
+
+        self.relu = nn.LeakyReLU(0.1)
+        self.pool = nn.MaxPool2d(2, 2)
 
         self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
@@ -55,20 +58,28 @@ class PriyamDigitNet(nn.Module):
         self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
         self.bn3 = nn.BatchNorm2d(128)
 
-        self.pool = nn.MaxPool2d(2, 2)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.4)
+        self.conv4 = nn.Conv2d(128, 256, 3, padding=1)
+        self.bn4 = nn.BatchNorm2d(256)
 
-        self.fc1 = nn.Linear(128 * 4 * 4, 512)
-        self.fc2 = nn.Linear(512, 10)
+        self.dropout_conv = nn.Dropout2d(0.3)
+        self.dropout_fc = nn.Dropout(0.5)
+
+        self.fc1 = nn.Linear(256 * 2 * 2, 256)
+        self.fc2 = nn.Linear(256, num_classes)
 
     def forward(self, x):
+
         x = self.pool(self.relu(self.bn1(self.conv1(x))))
         x = self.pool(self.relu(self.bn2(self.conv2(x))))
         x = self.pool(self.relu(self.bn3(self.conv3(x))))
-        x = x.view(-1, 128 * 4 * 4)
-        x = self.dropout(self.relu(self.fc1(x)))
+        x = self.pool(self.relu(self.bn4(self.conv4(x))))
+
+        x = self.dropout_conv(x)
+        x = x.view(x.size(0), -1)
+
+        x = self.dropout_fc(self.relu(self.fc1(x)))
         x = self.fc2(x)
+
         return x
 
 # ==============================
